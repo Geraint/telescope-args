5208e40b24dd57d80597509b49011db65de23d6c
Geraint Hywel <g.s.j.hywel@gmail.com>
2024-04-13 12:45:36 +0100

    Initial commit

---
+++ lua/telescope/_extensions/args.lua
@@ -0,0 +1,20 @@
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local conf = require("telescope.config").values
+
+local run = function(opts)
+  opts = opts or {}
+  pickers.new(opts, {
+    prompt_title = "args",
+    finder = finders.new_table {
+      results = vim.fn.argv(),
+    },
+    sorter = conf.generic_sorter(opts),
+  }):find()
+end
+
+return require("telescope").register_extension {
+  exports = {
+    args = run
+  },
+}
